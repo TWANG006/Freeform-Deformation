df49fe7456b54fe1ce5c12b28185745d649cd9fe
TWANG006 <twang006@e.ntu.edu.sg>
2016-10-11 16:59:31 +0800

    Add License

--- FFD_Local/FFD_Local/GluiSetup.h
+++ FFD_Local/FFD_Local/GluiSetup.h
@@ -1,3 +1,23 @@
+﻿/*********************************************************************
+ * Copyright © 2012-2016,
+ * Elysia Ong Wei Xi,
+ * WANG Tianyi: <tianyiwang666@gmail.com>
+ *
+ * This application (FFD) is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ ********************************************************************/
+
 #ifndef gluisetup_h
 #define gluisetup_h
 
